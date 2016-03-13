67cd2e3ca2ef9a9545b7dc54a1a4bc5ff1a452c5
Администратор <venil8@i.ua>
2016-03-13 12:54:14 +0200

    13.03.16   12-53   venil8

--- Tasks-2016-03-13/task1.py
+++ Tasks-2016-03-13/task1.py
@@ -7,7 +7,12 @@ class Book:
 
         Задание: сохраните title, year и genre в виде
         атрибутов объекта self
+        :rtype: object
         """
+        self.title = title
+        self.year = year
+        self.genre = genre
+
 
     def __repr__(self):
         """Возвращает внутреннее строковое представление объекта
@@ -16,6 +21,7 @@ class Book:
         Задание: верните строку в формате
                  Book("название", год, "жанр")
         """
+        return 'Book({!r},{!r},{!r})'.format(self.title, self.year, self.genre)
 
     def __str__(self):
         """Возвращает строку, предназначенную для пользователя
@@ -23,7 +29,16 @@ class Book:
 
         Задание: верните строку в произвольном формате
         """
+        return 'Book-! ({!r},{!r},{!r})'.format(self.title, self.year, self.genre)
 
 
 # Задание: создайте несколько объектов класса Book
 # и выведите их на экран
+a1 = Book("Fas", 1998, "df")
+b1 = Book("dc", "2", "d")
+print(a1)
+print(b1)
+
+
+
+
